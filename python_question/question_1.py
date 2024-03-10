7e3f9b7815f01e38101c7fcded84ccb405be4900
Akash vaishnav <aakashrankawat19@gmail.com>
2024-03-10 10:13:40 +0530

    completed question 1

---
+++ python_question/question_1.py
@@ -0,0 +1,38 @@
+class Question1_py:
+    def __init__(self, input_dict):
+        self.input_dict = input_dict
+
+    def extract_keys(self):
+        self.keys_list = []
+
+        def recursive_helper(current_dict):
+            for key, value in current_dict.items():
+                self.keys_list.append(key)
+                if isinstance(value, dict):
+                    recursive_helper(value)
+
+        recursive_helper(self.input_dict)
+        return self.keys_list
+
+    def final_value(self):
+        current_dict = self.input_dict
+        while isinstance(current_dict, dict):
+            key = next(iter(current_dict))
+            current_dict = current_dict[key]
+        return current_dict
+
+    def print_dict(self):
+        keys_list = self.extract_keys()
+        output = {}
+        for i, key in enumerate(keys_list):
+            if i < len(keys_list) - 1:
+                output[key] = keys_list[i+1:]
+            else:
+                output[key] = [self.final_value()]
+
+        for key, value in output.items():
+            print(f'"{key}":', value)
+
+
+# Example usage:
+
--- test.py
+++ test.py
@@ -1,2 +1,28 @@
-# its me 
-# again testing
+def convert_dict(input_dict):
+    output = {}
+    stack = []
+
+    def recursive_helper(current_dict):
+        for key, value in current_dict.items():
+            if isinstance(value, dict):
+                stack.append(key)
+                recursive_helper(value)
+                stack.pop()
+            else:
+                new_key = stack + [key]
+                if stack:
+                    parent_key = stack[-1]
+                    if parent_key not in output:
+                        output[parent_key] = []
+                    output[parent_key].append(new_key[-1])
+                else:
+                    output[new_key[-1]] = value
+
+    recursive_helper(input_dict)
+    return output
+
+input_dict = {"abc":{"def":{"ghi":{"jkl":{"mno":{"pqr":{"stu":{"vwx":{"yz":"you are finally here !!!"}}}}}}}}}
+output = convert_dict(input_dict)
+print(output)
+
+
